9af6489fe5b1c6f0477d3632f954c261e99072f8
Ubon Ndon <ubonndon@yahoo.com>
2022-06-20 01:32:54 -0500

    ain

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
 
 let content = document.querySelector('.js-generated.content')
 
-let header = document.createElement('h1')
-header.setAttribute('class', 'dog-name')
-header.append('Rizzo')
-content.append(header)
+let dogName = document.createElement('h1');
+dogName.textContent = 'Rizzo';
+dogName.setAttribute('class', 'dog-name');
+content.append(dogName);
 
-let dogContent = document.createElement('div')
-dogContent.setAttribute('class', 'dog-content')
+let dogContent = document.createElement('div');
+dogContent.setAttribute('class', 'dog-content');
 content.append(dogContent)
-
-let dogImage = document.createElement('img')
-dogImage.setAttribute('class', 'dog-image')
-dogImage.setAttribute('src', './assets/rizzo.jpg')
-content.append(dogImage)
-
-let dogDetails = document.createElement('div')
-dogDetails.setAttribute('class', 'dog-details')
-content.append(dogDetails)
 
 
